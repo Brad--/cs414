3c7ceb10e1697f5e02f9ba13475f17070e9c4a38
gphoon <garrett.phoonswadi@gmail.com>
2016-11-15 23:29:23 -0700

    added in the card class and modified board to set the positions to cards

---
+++ src/cs414/a5/groupA/monopoly/server/Card.java
@@ -0,0 +1,14 @@
+package cs414.a5.groupA.monopoly.server;
+
+/**
+ * Created by Garrett on 11/15/2016.
+ */
+public class Card extends Space {
+    public Card(Board board, int position){
+        super(board, position);
+    }
+    @Override
+    void action(Token target) {
+
+    }
+}
